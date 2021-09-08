51484169f10a434d077f8fba219f9929d52ddab6
Suraj Ahirwar <75873682+surajahirwar@users.noreply.github.com>
2021-09-08 14:26:25 +0530

    Add files via upload

--- mysite/settings.py
+++ mysite/settings.py
@@ -132,6 +132,6 @@ EMAIL_BACKEND   =   'django.core.mail.backends.console.EmailBackend'
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'surajifastdigital@gmail.com'
-EMAIL_HOST_PASSWORD = 'password'
+EMAIL_HOST_PASSWORD = 'surajsunita@!@#'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
